36655c679d0693d649c09f1c8ec106662117bf56
Ludwig Walther <lcwalther@gmail.com>
2019-03-13 12:59:50 +0100

    Add PageTitle component

--- src/upload/UploadPage.js
+++ src/upload/UploadPage.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import styled from 'styled-components'
+import PageTitleFullscreen from '../common/PageTitleFullscreen'
 
-const Grid = styled.div`
+const Grid = styled.section`
   display: grid;
+  grid-auto-rows: auto;
+  width: 100%;
+  margin: auto 0;
+  grid-gap: 20px;
+  padding: 20px;
 `
 
 export default function UploadPage() {
   return (
     <Grid>
-      <div>Title</div>
+      <PageTitleFullscreen>Upload video</PageTitleFullscreen>
     </Grid>
   )
 }
