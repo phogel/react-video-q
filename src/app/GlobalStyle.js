a0aedb7f21e7937d9b658d3f6d5fd8267d02d003
Ludwig Walther <lcwalther@gmail.com>
2019-03-13 13:10:09 +0100

    Update GlobalStyle

--- src/app/GlobalStyle.js
+++ src/app/GlobalStyle.js
@@ -20,4 +20,100 @@ export default createGlobalStyle`
     font-family: 'Dosis', sans-serif;
     margin: 0;
   }
+
+  button, input[type=submit] {
+    position: relative;
+    padding: 12px 24px;
+    overflow: hidden;
+    border-width: 0;
+    outline: none;
+    border-radius: 2px;
+    box-shadow: 0 1px 4px rgba(0, 0, 0, .6);
+    font-size: 14px;
+    text-transform: uppercase;
+    height: 40px;
+    width: 100%;
+    background: #1a1a1a;
+    color: rgb(250, 250, 250);
+    transition: background-color .3s;
+    :hover, :focus {
+      background-color: #FF328B;
+    }
+    > * {
+      position: relative;
+    }
+    span {
+      display: block;
+      padding: 12px 24px;
+    }
+    :before {
+      content: "";
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      display: block;
+      width: 0;
+      padding-top: 0;
+      border-radius: 100%;
+      background-color: rgba(236, 240, 241, .3);
+      transform: translate(-50%, -50%);
+    }
+    :active:before {
+      width: 120%;
+      padding-top: 120%;
+      transition: width .2s ease-out, padding-top .2s ease-out;
+    }
+  }
+
+  textarea:focus, input:focus{
+    outline: none;
+  }
+
+  .form__group {
+  position: relative;
+  padding: 15px 0 0;
+}
+
+.form__field {
+  margin-bottom: 20px;
+  font-family: inherit;
+  width: 100%;
+  border: 0;
+  border-bottom: 1px solid #d2d2d2;
+  outline: 0;
+  font-size: 16px;
+  color: #212121;
+  padding: 7px 0;
+  background: transparent;
+  transition: border-color 0.2s;
+}
+
+.form__field::placeholder {
+  color: transparent;
+}
+
+.form__field:placeholder-shown ~ .form__label {
+  font-size: 16px;
+  cursor: text;
+  top: 20px;
+}
+
+label,
+.form__field:focus ~ .form__label {
+  position: absolute;
+  top: 0;
+  display: block;
+  transition: 0.2s;
+  font-size: 12px;
+  color: #9b9b9b;
+}
+
+.form__field:focus ~ .form__label {
+  color: #FF328B;
+}
+
+.form__field:focus {
+  padding-bottom: 6px;
+  border-bottom: 2px solid #FF328B;
+}
 `
--- src/upload/UploadPage.js
+++ src/upload/UploadPage.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 import PageTitleFullscreen from '../common/PageTitleFullscreen'
 
@@ -12,9 +13,31 @@ const Grid = styled.section`
 `
 
 export default function UploadPage() {
+  function goBack() {
+    window.history.back()
+  }
+
   return (
     <Grid>
       <PageTitleFullscreen>Upload video</PageTitleFullscreen>
+      <form className="form__group">
+        <input className="form__field" type="text" placeholder="Title" />
+        <input
+          className="form__field"
+          type="text"
+          placeholder="Tags: e.g. head roll, Pablo & Raquel, 2018..."
+        />
+        <input className="form__field" type="text" placeholder="Video notes" />
+        <input
+          className="form__field"
+          type="text"
+          placeholder="YouTube video ID"
+        />
+        <button>Upload</button>
+      </form>
+      <Link to="" onClick={goBack}>
+        Cancel
+      </Link>
     </Grid>
   )
 }
