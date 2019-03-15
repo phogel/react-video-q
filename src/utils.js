297cb147c016d1a8829a92225332fbbaee7f9be5
Ludwig Walther <lcwalther@gmail.com>
2019-03-15 11:49:04 +0100

    Fix edit function bug

--- src/utils.js
+++ src/utils.js
@@ -1,30 +1,20 @@
 export default function split(str) {
-  return str
-    ? str
-        .join()
-        .split(',')
-        .map(item => item.trim())
-        .filter(item => item.length)
-    : []
+  if (typeof str === 'string') {
+    return str
+      ? str
+          .split(',')
+          .map(item => item.trim())
+          .filter(item => item.length)
+      : []
+  } else if (typeof str === 'object') {
+    return str
+      ? str
+          .join()
+          .split(',')
+          .map(item => item.trim())
+          .filter(item => item.length)
+      : []
+  } else {
+    return str
+  }
 }
-
-// export default function split(str) {
-//   if (typeof str === String) {
-//     return str
-//       ? str
-//           .split(',')
-//           .map(item => item.trim())
-//           .filter(item => item.length)
-//       : []
-//   } else if (typeof str === Array) {
-//     return str
-//       ? str
-//           .join()
-//           .split(',')
-//           .map(item => item.trim())
-//           .filter(item => item.length)
-//       : []
-//   } else {
-//     return str
-//   }
-// }
