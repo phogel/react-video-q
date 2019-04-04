9134e6499fefd3cd497fd9369ff1fe3c7fa7bf0d
D. Klevinghaus <adk@codekommando.de>
2019-04-04 15:36:36 +0200

    Send current server as origin

--- src/youtube/YouTubeVideoIframe.js
+++ src/youtube/YouTubeVideoIframe.js
@@ -32,7 +32,7 @@ export default function YouTubeVideo({
         title={videoId}
         width="100%"
         height="auto"
-        src={`https://www.youtube.com/embed/${videoId}?wmode=opaque&modestbranding=1&showinfo=0&rel=0&cc_load_policy=1&iv_load_policy=3&color=white&autohide=0&start=${startSeconds}&end=${endSeconds}`}
+        src={`https://www.youtube.com/embed/${videoId}?wmode=opaque&modestbranding=1&showinfo=0&rel=0&cc_load_policy=1&iv_load_policy=3&color=white&autohide=0&start=${startSeconds}&end=${endSeconds}&origin=${location.protocol+'//'+location.host}`}
         frameBorder="0"
         allow={`accelerometer; ${
           autoplay ? 'autoplay' : null
